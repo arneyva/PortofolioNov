32645b8ea52ba5a7fffbf4d2445a23b105a648ef
arneyva <princethereal1416@gmail.com>
2024-11-16 14:34:35 +0700

    update responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,20 +3,23 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
-      fontFamily:{
-        cyborg:["Cyborg"],
-        circular:["Circular Std"],
-        charmonman:["Charmonman Regular"],
+      fontFamily: {
+        cyborg: ["Cyborg"],
+        circular: ["Circular Std"],
+        charmonman: ["Charmonman Regular"],
       },
       colors: {
         ijo: "#0A5C33",
         ijonom: "#58AA25",
         grey: "#9E9E9E",
         coklit: "#8B644E",
-       
+        oren: "#F1582C",
+      },
+      dropShadow: {
+        myshadow: ["0px 4px 4px rgba(0, 0, 0, 0.25)"],
+        "xl-shadow": [" 0px 41px 89px 0px #0000001A"],
       },
     },
   },
   plugins: [],
-}
-
+};
